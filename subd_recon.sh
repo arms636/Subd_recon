70a8ff2ba6c23ba11965f617c1aa4373baccb97b
Abdul Rahiman M S <76870086+arms636@users.noreply.github.com>
2021-06-28 13:41:36 +0530

    Add files via upload

--- subd_recon.sh
+++ subd_recon.sh
@@ -9,6 +9,8 @@ green=`tput setaf 2`
 blue=`tput setaf 4`
 reset=`tput sgr0`
 bold=`tput bold`
+
+
 #Greetings
 greetings()
 {
@@ -16,32 +18,45 @@ echo -e "${red}${bold}Welcome $USER! Today is $day.\nYou are using $SHELL shell 
 }
 dom=$1
 read -p  "${red}${bold}Enter a domain : ${reset}" dom
+# mkdir dom
+Directory()
+{
+mkdir "$dom"
+touch $dom/subd.txt
+touch $dom/alive.txt
+touch $dom/sorted_subs.txt	
+}
 # Sub domain Recon #
 sub_recon()
 {
 echo "${blue}${bold}Subdomains of $dom is Loading${reset}.............."
-assetfinder -subs-only $dom > subd.txt
+assetfinder -subs-only $dom > $dom/subd.txt
 echo "${blue}${bold}Completed${reset}"
 
 echo "${blue}${bold}Live Subdomains of $dom is Loading${reset}..............."
-cat subd.txt | httprobe > alive.txt
+cat $dom/subd.txt | httprobe > $dom/alive.txt
 echo "${blue}${bold}Completed${reset}"
 
 # sorting subdomains
 while read url ; do
 	echo ${url#*//} >> urls.txt
-done < subd.txt
-sort -u urls.txt > sorted_subs.txt
-count=$(cat sorted_subs.txt | wc -l)
+done < $dom/subd.txt
+sort -u urls.txt > $dom/sorted_subs.txt
+count=$(cat $dom/sorted_subs.txt | wc -l)
 rm urls.txt
 }
 
 # Main Script #
 
+#if [[ -f '! ($pwd/subd)' ]]; then
+#	echo "${red}${bold}Subdomains Not Found${reset}"
+#else
 greetings
+Directory
 sub_recon
 echo "${red}${bold}Script Execution Completed${reset}"
 echo "${blue}${bold}Total ${red}${bold} ${count} ${reset}${blue}${bold} subdomains found.${reset}"
+#fi
 
 
 
